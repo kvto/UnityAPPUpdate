0fb6afcd4ab5896347f9df26dd3e8816cc3a7e1f
Kevin J. Monter Zea <kjmz.15987@gmail.com>
2023-05-23 16:47:51 -0500

    UnityAPPUpdate

---
+++ Assets/Script/Items.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+
+public class Items : ScriptableObject
+{
+    public string ItemName;
+    public Sprite ItemImage;
+    public string ItemDescription;
+    public GameObject Item3DModel;
+}
